83ee31179cd759d435049a8fc089b5b00a64ad33
breech oracion <oracionbreech@gmail.com>
2023-05-26 07:57:22 -0600

    navigate to weather route with city param

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,49 @@
-import { Box, Container, TextField, Typography } from '@mui/material'
+import { useAuth0 } from '@auth0/auth0-react';
+import { Search } from '@mui/icons-material';
+import { Box, Button, Container, FormControl, Input, InputAdornment, Typography } from '@mui/material'
 import React from 'react'
+import { getWeatherByCity } from '../services/weather';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const { user } = useAuth0()
+    const [city, setCity] = React.useState('');
+
+    const navigate = useNavigate();
+
+    const getWeather = async () => {
+        navigate(`/weather?city=${city}`)
+    }
+
     return (
         <Container>
             <Box justifyContent={'center'}>
                 <Typography textAlign={'center'} marginTop={'5%'}>
-                    John Smith
+                    {user?.nickname}
                 </Typography>
                 <Typography textAlign={'center'}>
-                    https://github.com/smithjohn
+                    https://github.com/{user?.nickname}
                 </Typography>
 
                 <Box justifyContent={'center'} display={'flex'} marginTop={'1%'}>
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                    <FormControl variant="outlined">
+                        <Input
+                            error={city.length === 0}
+                            startAdornment={
+                                <InputAdornment position="start">
+                                    <Search />
+                                </InputAdornment>
+                            }
+                            onChange={(e) => setCity(e.target.value)}
+                            placeholder='City'
+                        />
+                    </FormControl>
+                </Box>
+
+                <Box justifyContent={'center'} display={'flex'} marginTop={'1%'}>
+                    <Button disabled={city.length === 0} onClick={getWeather} variant="contained" sx={{
+                        marginTop: '2rem'
+                    }}>Display Weather</Button>
                 </Box>
             </Box>
         </Container>
