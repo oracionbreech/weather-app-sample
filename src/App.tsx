8c0eb1613e629f8aff38d671446d2934fffe7b12
breech oracion <oracionbreech@gmail.com>
2023-05-26 00:52:32 -0600

    added auth0 react as dep

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,29 @@ import Login from './pages/Login';
 import Layout from './components/layout';
 import { Provider } from 'react-redux';
 import store from './store';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </Provider>
+    <Auth0Provider
+    domain="http://localhost:3000"
+    clientId="aEyPo8dKb99bkIfUWywf2F5LUVEHFgTX"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    >
+
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route path='login' element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+
+    </Auth0Provider>
   );
 }
 
